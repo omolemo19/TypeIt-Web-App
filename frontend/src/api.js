44bedaff4c0268d7bb5c0ac96838b99c84f55f11
Omolemo Modise <omolemo.modise19@gmail.com>
2024-12-02 13:42:35 +0200

    added backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl = "/choreo-apis/typeit-fullstack/backend/v1"
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
